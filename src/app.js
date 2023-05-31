0bf4e8b358aad89b8f0df9760420c9799ffa9eb6
camilarsobral <camilarsobral@hotmail.com>
2023-05-31 16:13:04 -0300

    alteração cors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ import './database'
 import { resolve } from 'path'
 import cors from 'cors'
 
+const corsOption = {
+    origin: 'https://codeburger-frontend.vercel.app',
+    credentials: true
+}
+
 class App {
     constructor() {
         this.app = express()
-        this.app.use(cors())
+        this.app.use(cors(corsOption))
 
         this.middlewares()
         this.routes()
